4a7c1fff85cbb168ddbe706e9c1dcc88dc956e4d
Seth Karim Luis Martínez <seth.luis@banregio.com>
2022-05-22 18:04:42 -0500

    Ejemplo de directiva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { CabeceraComponent } from "./componentes/cabecera/cabecera.component";
 import { PieComponent } from "./componentes/pie/pie.component";
+import { DirectivaComponent } from "./directiva/directiva.component";
 
 @NgModule({
-  declarations: [AppComponent, CabeceraComponent, PieComponent],
+  declarations: [
+    AppComponent,
+    CabeceraComponent,
+    PieComponent,
+    DirectivaComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+export class DirectivaComponent {
+
+  listaCursos:string[] = ['Typescript', 'Java', 'C#']
+}
