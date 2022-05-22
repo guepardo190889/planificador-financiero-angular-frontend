8bc120b334869356f281f6ea82cb38371e4ca8be
Seth Karim Luis Martínez <seth.luis@banregio.com>
2022-05-22 17:37:07 -0500

    Se crea componente de cabecera

---
+++ src/app/componentes/cabecera/cabecera.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'cabecera',
+    templateUrl:'./cabecera.component.html'
+})
+export class CabeceraComponent {
+    titulo:string = 'Planificador Financiero';
+}
