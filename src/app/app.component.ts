34ea9d89689c8788831c7a94d72b899e91eb89af
Seth Karim Luis Martínez <seth.luis@banregio.com>
2022-05-22 17:08:50 -0500

    Se limpia index.html y se añade texto de ejmplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'planificador-financiero-angular-frontend';
+  title:string = 'Bienvenido al Planificador financiero';
+  tituloDivisa:string = "Divisas"; 
+  tituloCategoria:string = "Categorías"
 }
