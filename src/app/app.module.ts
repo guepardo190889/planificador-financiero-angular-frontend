c19209883b1a2e25c3d91ba514b826267d07f196
Seth Karim Luis Martínez <seth.luis@banregio.com>
2022-05-22 17:57:35 -0500

    Se crea componente de pie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { CabeceraComponent } from "./componentes/cabecera/cabecera.component";
+import { PieComponent } from "./componentes/pie/pie.component";
 
 @NgModule({
-  declarations: [AppComponent, CabeceraComponent],
+  declarations: [AppComponent, CabeceraComponent, PieComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
