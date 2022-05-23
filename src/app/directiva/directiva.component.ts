20baaa3fa4294f84c83ccf9ed51bbf99d269b5af
Seth Karim Luis Martínez <seth.luis@banregio.com>
2022-05-22 21:45:25 -0500

    Se agrega botón con métodos bindeados al typescript

--- src/app/directiva/directiva.component.ts
+++ src/app/directiva/directiva.component.ts
@@ -7,4 +7,10 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivaComponent {
 
   listaCursos:string[] = ['Typescript', 'Java', 'C#']
+
+  mostrarBoton:boolean = true;
+
+  habilitarBoton():void {
+    this.mostrarBoton = !this.mostrarBoton;
+  }
 }
