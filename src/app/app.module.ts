71e0d2e6beb10bb0b0b1354015ba6ed5f851d6e8
Seth Karim Luis Martínez <seth.luis@banregio.com>
2022-05-22 21:49:58 -0500

    Se mueve de directorio directiva y se crea componente cliente y clase cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { CabeceraComponent } from "./componentes/cabecera/cabecera.component";
 import { PieComponent } from "./componentes/pie/pie.component";
-import { DirectivaComponent } from "./directiva/directiva.component";
+import { DirectivaComponent } from "./componentes/directiva/directiva.component";
+import { DivisaComponent } from './componentes/divisa/divisa.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { DirectivaComponent } from "./directiva/directiva.component";
     CabeceraComponent,
     PieComponent,
     DirectivaComponent,
+    DivisaComponent,
   ],
   imports: [BrowserModule],
   providers: [],
---
+++ src/app/componentes/divisa/divisa.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-divisa',
+  templateUrl: './divisa.component.html'
+})
+export class DivisaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
