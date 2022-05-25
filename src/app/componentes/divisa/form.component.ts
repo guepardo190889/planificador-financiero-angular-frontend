62b852fac3dafad2f52b2443559a509ed4fe3099
Seth Karim Luis Martínez <seth.luis@banregio.com>
2022-05-25 12:27:12 -0500

    Se integra seetalert2

--- src/app/componentes/divisa/form.component.ts
+++ src/app/componentes/divisa/form.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { DivisaService } from 'src/app/servicios/divisa.service';
-import { Divisa } from './divisa';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { DivisaService } from "src/app/servicios/divisa.service";
+import Swal from "sweetalert2";
+import { Divisa } from "./divisa";
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html'
+  selector: "app-form",
+  templateUrl: "./form.component.html",
 })
 export class FormComponent implements OnInit {
-  private titulo:string = "Guardar Divisa"
-  private divisa:Divisa = new Divisa();
+  private titulo: string = "Guardar Divisa";
+  private divisa: Divisa = new Divisa();
 
-  constructor(private divisaService:DivisaService, private router:Router) { }
+  constructor(private divisaService: DivisaService, private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public guardar(): void {
+    this.divisaService.guardar(this.divisa).subscribe((divisaGuardada) => {
+      this.router.navigate(["/divisas"]);
+      Swal.fire("Nueva Divisa", `Divisa ${divisaGuardada.nombre}`, "success");
+    });
   }
-
-  public guardar():void {
-    this.divisaService.guardar(this.divisa).subscribe(
-      response => this.router.navigate(['/divisas'])
-    );
-  }
-
 }
