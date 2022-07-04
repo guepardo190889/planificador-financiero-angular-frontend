f267454b438cae5b3360c5c98057161c86baf2f6
Seth Karim Luis Martínez <seth.luis@banregio.com>
2022-07-03 22:12:49 -0500

    Formato aceptable

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -28,20 +28,25 @@ export class InicioComponent implements OnInit {
 
   inicializarCategorias(): void {
     this.categorias.push(new CategoriaDD(1, "Categoría 1     ", 0));
+    this.categorias.push(new CategoriaDD(1, "————————————————", 0));
     this.categorias.push(new CategoriaDD(2, "Categoría 2     ", 0));
-    this.categorias.push(new CategoriaDD(3, "— —Categoría 2.1", 0));
+    this.categorias.push(new CategoriaDD(3, "—— Categoría 2.1", 0));
+    this.categorias.push(new CategoriaDD(1, "————————————————", 0));
     this.categorias.push(new CategoriaDD(4, "Categoría 3     ", 0));
-    this.categorias.push(new CategoriaDD(5, "— —Categoría 3   ", 0));
-    this.categorias.push(new CategoriaDD(6, "— —Categoría 3.1  ", 0));
-    this.categorias.push(new CategoriaDD(7, "— —Categoría 3.1", 0));
-    this.categorias.push(new CategoriaDD(8, "— — —Categoría 3.1.1", 0));
-    this.categorias.push(new CategoriaDD(9, "— — —Categoría 3.1.2", 0));
-    this.categorias.push(new CategoriaDD(10, "— —Categoría 3.2", 0));
-    this.categorias.push(new CategoriaDD(11, "— —Categoría 3.3", 0));
+    this.categorias.push(new CategoriaDD(5, "—— Categoría 3   ", 0));
+    this.categorias.push(new CategoriaDD(6, "—— Categoría 3.1  ", 0));
+    this.categorias.push(new CategoriaDD(7, "—— Categoría 3.1", 0));
+    this.categorias.push(new CategoriaDD(8, "—— —— Categoría 3.1.1", 0));
+    this.categorias.push(new CategoriaDD(9, "—— —— Categoría 3.1.2", 0));
+    this.categorias.push(new CategoriaDD(10, "—— Categoría 3.2", 0));
+    this.categorias.push(new CategoriaDD(11, "—— Categoría 3.3", 0));
+    this.categorias.push(new CategoriaDD(1, "————————————————", 0));
     this.categorias.push(new CategoriaDD(12, "Categoría 4", 0));
-    this.categorias.push(new CategoriaDD(13, "— —Categoría 4.1", 0));
-    this.categorias.push(new CategoriaDD(13, "— — —Categoría 4.1.1", 0));
+    this.categorias.push(new CategoriaDD(13, "—— Categoría 4.1", 0));
+    this.categorias.push(new CategoriaDD(13, "—— —— Categoría 4.1.1", 0));
+    this.categorias.push(new CategoriaDD(1, "————————————————", 0));
     this.categorias.push(new CategoriaDD(12, "Categoría 5", 0));
+    this.categorias.push(new CategoriaDD(1, "————————————————", 0));
   }
 
   guardar(): void {
